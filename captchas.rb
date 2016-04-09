add9ecd7e0cee60cc08a845aa35095e3d9a49f43
Alexey Vasilyev <bestspajic@gmail.com>
2016-04-09 16:04:24 +0300

    Fix some rubocop warnings

--- captchas.rb
+++ captchas.rb
@@ -9,9 +9,12 @@ class RuCaptchaSolver
   def solve(path_to_captcha_image_file)
     puts 'REQUEST RUCAPTCHA'
     path_to_captcha = File.expand_path path_to_captcha_image_file
-    captcha_id = @api.send_captcha_for_solving(path_to_captcha, params: {language: 2})
+    captcha_id = @api.send_captcha_for_solving(
+      path_to_captcha,
+      params: { language: 2 }
+    )
     solved_captcha = @api.get_solved_captcha captcha_id
-    puts "RUCAPTHCA REQUEST SUCCESS"
+    puts 'RUCAPTHCA REQUEST SUCCESS'
     solved_captcha.upcase
   end
 end
@@ -23,17 +26,18 @@ class CaptchaSolverByHand
   end
 
   private
-    def show_capthca(path_to_captcha_image_file)
-      Launchy.open(path_to_captcha_image_file)
-    end
 
-    def ask_hand_input
-      show_capthca(path_to_captcha_image_file)
-      puts "Enter Capthca value, please: "
-    end
+  def show_capthca(path_to_captcha_image_file)
+    Launchy.open(path_to_captcha_image_file)
+  end
 
-    def get_hand_input
-      res = gets
-      res.chomp!
-    end
+  def ask_hand_input
+    show_capthca(path_to_captcha_image_file)
+    puts 'Enter Capthca value, please: '
+  end
+
+  def get_hand_input
+    res = gets
+    res.chomp!
+  end
 end
