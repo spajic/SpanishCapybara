9cf13d8a7ccd2d7a21afcbf47b2aff9dfecab1f5
Alexey Vasilyev <bestspajic@gmail.com>
2016-10-16 17:51:46 +0300

    Pass 'phrase: 1' to RuCaptcha API

--- captcha_solver_rucaptcha.rb
+++ captcha_solver_rucaptcha.rb
@@ -11,11 +11,14 @@ class CaptchaSolverRucaptcha
     captcha_id = @api.send_captcha_for_solving(
       File.expand_path(path_to_captcha_image_file),
       params: {
-        language: 2 # English
+        language: 2, # English
+        phrase: 1, # Капча из двух слов
       }
     )
     solved_captcha = @api.get_solved_captcha(captcha_id)
     puts 'RUCAPTHCA REQUEST SUCCESS'
+    puts solved_captcha
+    puts '*****************************************'
     solved_captcha.upcase
   end
 end
